2f12a52250234ea3c7b938ac7598d121bb175f2e
emanuelcgalindo@hotmail.com <emanuelcgalindo@hotmail.com>
2024-06-24 10:50:17 -0300

    4 commit

--- src/Enderecos.java
+++ src/Enderecos.java
@@ -1,3 +1,15 @@
-public class Endereco {
+public class Enderecos {
+   private String rua;
+   private String cidade;
+   private String estado;
 
+   public Enderecos(String rua, String cidade, String estado) {
+    this.rua = rua;
+    this.cidade = cidade;
+    this.estado = estado;
+
+
+
+
+   }
 }
