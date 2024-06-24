8e6dd54c73b99195998c8340eb7ef5019e9e5df5
emanuelcgalindo@hotmail.com <emanuelcgalindo@hotmail.com>
2024-06-24 10:40:16 -0300

    mais um commit

---
+++ src/Pessoas.java
@@ -0,0 +1,26 @@
+public class Pessoas {
+    private String nome;
+    private int idade;
+    private int codigo;
+
+    public Pessoas(String nome, int idade, int codigo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.codigo = codigo;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+
+    }
+
+    public int codigo(){
+        return codigo;
+    }
+
+
+}
